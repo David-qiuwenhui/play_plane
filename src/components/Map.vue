6f34d851111e8cc8681fdcafe9a00c9b93363afe
qiuwenhui <1059713136@qq.com>
2024-08-25 12:05:22 +0800

    🦄 refactor(GamePage): 优化Map组件的组成形式

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,19 +1,60 @@
 <template>
   <container>
-    <sprite :texture="MapImg"></sprite>
+    <sprite :texture="MapImg" :y="mapY1"></sprite>
+    <sprite :texture="MapImg" :y="mapY2"></sprite>
   </container>
 </template>
 
 <script>
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { game } from "../game";
 import MapImg from "../assets/map.jpg";
 
 export default {
-  setup(props, { emit }) {
+  setup() {
+    const { mapY1, mapY2 } = useMove();
+
     return {
       MapImg,
+      mapY1,
+      mapY2,
     };
   },
 };
+
+// 更新图片坐标的逻辑
+function useMove() {
+  const gameHeight = 1080;
+  const mapY1 = ref(0);
+  const mapY2 = ref(-gameHeight);
+
+  const speed = 10;
+  function handleTicker() {
+    mapY1.value += speed;
+    mapY2.value += speed;
+
+    if (mapY1.value >= gameHeight) {
+      mapY1.value = -gameHeight;
+    }
+
+    if (mapY2.value >= gameHeight) {
+      mapY2.value = -gameHeight;
+    }
+  }
+
+  onMounted(() => {
+    game.ticker.add(handleTicker);
+  });
+
+  onBeforeUnmount(() => {
+    game.ticker.remove(handleTicker);
+  });
+
+  return {
+    mapY1,
+    mapY2,
+  };
+}
 </script>
 
 <style scoped></style>
--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -1,60 +1,19 @@
 <template>
   <container>
-    <Map :y="mapY1"></Map>
-    <Map :y="mapY2"></Map>
+    <Map></Map>
   </container>
 </template>
 
 <script>
-import { ref, onMounted, onBeforeUnmount } from "vue";
 import Map from "../components/Map.vue";
-import { game } from "../game";
 export default {
   components: {
     Map,
   },
   setup() {
-    const { mapY1, mapY2 } = useMove();
-    return {
-      mapY1,
-      mapY2,
-    };
+    return {};
   },
 };
-
-// 更新图片坐标的逻辑
-function useMove() {
-  const gameHeight = 1080;
-  const mapY1 = ref(0);
-  const mapY2 = ref(-gameHeight);
-
-  const speed = 10;
-  function handleTicker() {
-    mapY1.value += speed;
-    mapY2.value += speed;
-
-    if (mapY1.value >= gameHeight) {
-      mapY1.value = -gameHeight;
-    }
-
-    if (mapY2.value >= gameHeight) {
-      mapY2.value = -gameHeight;
-    }
-  }
-
-  onMounted(() => {
-    game.ticker.add(handleTicker);
-  });
-
-  onBeforeUnmount(() => {
-    game.ticker.remove(handleTicker);
-  });
-
-  return {
-    mapY1,
-    mapY2,
-  };
-}
 </script>
 
 <style scoped></style>
